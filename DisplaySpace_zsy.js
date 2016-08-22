f46d1989560fb47814ee079c06f240ba1c1cded3
zhusiyi111 <625700571@qq.com>
2016-08-22 14:33:20 +0800

    空格插件（jQuery） V0.1

---
+++ DisplaySpace_zsy.js
@@ -0,0 +1,47 @@
+
+(function($){
+    $.fn.DisplaySpace = function(){
+
+        this.each(function(){
+
+           $(this).on('input propertychange', function () {
+
+                var str = $(this).val();
+                str = filterStr(str);
+                var result = '';
+                for(var i=0;i<str.length;i++){
+                    if(i>=11)
+                    {
+                        break;
+                    }
+                    if(3==i || 7==i){
+                        result += ' ';
+                     }
+                    result += str[i];
+                }
+               $(this).val(result);
+
+
+           })
+
+            function filterStr(str){
+                var result='';
+                for(var i=0;i<str.length;i++){
+                    if(!isNaN(str[i])){
+                        if(str[i]!=' '){
+                            result+=str[i];
+                        }
+                    }
+                }
+                return result;
+            }
+
+
+
+
+
+
+
+        });
+    };
+})(jQuery);
